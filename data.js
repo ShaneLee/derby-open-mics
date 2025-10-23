865b11cbf1e339a9627f4b188b27c76b8bed95c0
Shane <me@shanel.ee>
2025-10-23 18:35:50 +0100

    Automatic data update: 2025-10-23 18:35:50

--- data.js
+++ data.js
@@ -1,93 +1,56 @@
 const eventsData = [
-  {
-    "name": "Darley Abbey Wines Open Mic Night",
-    "notes": "",
-    "date": "2025-10-23",
-    "startTime": "17:00:00",
-    "endTime": "23:00:00"
-  },
-  {
-    "name": "The Prince Of Wales Open Mic Night",
-    "notes": "Hosted by Sam",
-    "date": "2025-10-23",
-    "startTime": "19:30:00",
-    "endTime": null
-  },
-  {
-    "name": "The Royal Telegraph Open Mic Night",
-    "notes": "",
-    "date": "2025-10-27",
-    "startTime": "19:00:00",
-    "endTime": "22:00:00"
-  },
-  {
-    "name": "Love Your Sound Open Mic (Bunk)",
-    "notes": "https://www.derbycathedralquarter.co.uk/events/love-your-sound-open-mic/",
-    "date": "2025-10-28",
-    "startTime": "19:00:00",
-    "endTime": "23:00:00"
-  },
-  {
-    "name": "Love Your Sound Open Mic (Bunk)",
-    "notes": "https://www.derbycathedralquarter.co.uk/events/love-your-sound-open-mic/",
-    "date": "2025-10-28",
-    "startTime": "19:00:00",
-    "endTime": "23:00:00"
-  },
-  {
-    "name": "The Creaky Floorboard Open Mic",
-    "notes": "",
-    "date": "2025-10-29",
-    "startTime": "19:30:00",
-    "endTime": "23:00:00"
-  },
-  {
-    "name": "The Royal Standard Open Mic Night",
-    "notes": "Hosted by Ding",
-    "date": "2025-10-29",
-    "startTime": "19:30:00",
-    "endTime": null
-  },
-  {
-    "name": "Open Mic Night Derby Market Hall",
-    "notes": "",
-    "date": "2025-10-30",
-    "startTime": "18:00:00",
-    "endTime": "21:45:00"
-  },
-  {
-    "name": "Royal Telegraph Open Mic",
-    "notes": "",
-    "date": "2025-11-03",
-    "startTime": "19:00:00",
-    "endTime": "22:00:00"
-  },
-  {
-    "name": "Love Your Sound Open Mic (Bunk)",
-    "notes": "https://www.derbycathedralquarter.co.uk/events/love-your-sound-open-mic/",
-    "date": "2025-11-04",
-    "startTime": "19:00:00",
-    "endTime": "23:00:00"
-  },
-  {
-    "name": "Royal Telegraph Open Mic",
-    "notes": "",
-    "date": "2025-11-10",
-    "startTime": "19:00:00",
-    "endTime": "22:00:00"
-  },
-  {
-    "name": "Love Your Sound Open Mic (Bunk)",
-    "notes": "https://www.derbycathedralquarter.co.uk/events/love-your-sound-open-mic/",
-    "date": "2025-11-11",
-    "startTime": "19:00:00",
-    "endTime": "23:00:00"
-  },
-  {
-    "name": "Bless Open Mic",
-    "notes": "Not sure of the time",
-    "date": "2025-11-27",
-    "startTime": "00:00:00",
-    "endTime": "00:00:00"
-  }
-]
+    {
+        "id": "1f46eff8-6d34-390e-93de-946b65db940b",
+        "name": "Darley Abbey Wines Open Mic Night",
+        "category": "Open Mic",
+        "notes": "",
+        "date": "2025-10-23",
+        "startTime": "17:00:00",
+        "endTime": "23:00:00"
+    },
+    {
+        "id": "7c62e5ae-7f95-3806-85b4-34a3fb9c4cbe",
+        "name": "The Prince Of Wales Open Mic Night",
+        "category": "Open Mic",
+        "notes": "Hosted by Sam",
+        "date": "2025-10-23",
+        "startTime": "19:30:00",
+        "endTime": null
+    },
+    {
+        "id": "14c996bc-ed0b-3915-b133-1a3b31b306f4",
+        "name": "The Royal Telegraph Open Mic Night",
+        "category": "Open Mic",
+        "notes": "",
+        "date": "2025-10-27",
+        "startTime": "19:00:00",
+        "endTime": "22:00:00"
+    },
+    {
+        "id": "ddc093f1-1425-3aae-b1bc-9ab67b8826b3",
+        "name": "Love Your Sound Open Mic (Bunk)",
+        "category": "Open Mic",
+        "notes": "https://www.derbycathedralquarter.co.uk/events/love-your-sound-open-mic/",
+        "date": "2025-10-28",
+        "startTime": "19:00:00",
+        "endTime": "23:00:00"
+    },
+    {
+        "id": "219fc5a7-aee3-3936-9f40-2ef188e8402d",
+        "name": "The Creaky Floorboard Open Mic",
+        "category": "Open Mic",
+        "notes": "",
+        "date": "2025-10-29",
+        "startTime": "19:30:00",
+        "endTime": "23:00:00"
+    },
+    {
+        "id": "a52460ae-7595-377f-9ade-565292815831",
+        "name": "The Royal Standard Open Mic Night",
+        "category": "Open Mic",
+        "notes": "Hosted by Ding",
+        "date": "2025-10-29",
+        "startTime": "19:30:00",
+        "endTime": null
+    }
+];
